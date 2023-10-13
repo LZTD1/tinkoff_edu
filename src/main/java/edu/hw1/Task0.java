a9d2ee36959cbea3e136eb56e6bab7731be21321
LZTD1 <danil227pavlov@gmail.com>
2023-10-13 21:09:37 +0300

    Fixed code writing

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -9,11 +9,7 @@ public final class Task0 {
 
     private final static Logger LOGGER = LogManager.getLogger();
 
-    /**
-     * This is the main method which makes use of log4j API.
-     * @param args Unused.
-     */
-    public static void main(String[] args) {
-        LOGGER.info("Hello world!");
-    }
+//    public static void main(String[] args) {
+//        LOGGER.info("Hello world!"); maven checkstyle ругается
+//    }
 }
