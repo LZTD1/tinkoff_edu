447281e6e5fe7b06fb99cc5360d7d8ead2c224b7
LZTD1 <danil227pavlov@gmail.com>
2023-10-25 11:55:08 +0300

    Реализация 4 задания

---
+++ src/main/java/edu/Task4.java
@@ -0,0 +1,71 @@
+package edu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task4 {
+
+    private final static int FIVE = 5;
+    private final static int THREE = 3;
+
+    private Task4() {
+    }
+
+    public static String convertToRoman(int digits) {
+        String[] rom = new String[] {
+            "I", // 1
+            "V", // 5
+            "X", // 10
+            "L", // 50
+            "C", // 100
+            "D", // 500
+            "M", // 1000
+        };
+        List<List<String>> numbers = new ArrayList<>() {{
+            add(new ArrayList<>() {{
+                add("0");
+                add("1");
+                add("2");
+                add("3");
+            }});
+            add(new ArrayList<>() {{
+                add("4");
+            }});
+            add(new ArrayList<>() {{
+                add("5");
+                add("6");
+                add("7");
+                add("8");
+            }});
+            add(new ArrayList<>() {{
+                add("9");
+            }});
+        }};
+        String answer = "";
+        char[] reversedString = new StringBuilder(Integer.toString(digits))
+            .reverse()
+            .toString()
+            .toCharArray();
+        int romCounter = 0;
+
+        for (int i = 0; i < reversedString.length; i++) {
+            String currentCharAsString = String.valueOf(reversedString[i]);
+
+            if (numbers.get(0).contains(currentCharAsString)) { // 0, 1, 2, 3
+                answer = rom[romCounter].repeat(Integer.parseInt(String.valueOf(reversedString[i])))
+                    + answer;
+            } else if (numbers.get(1).contains(currentCharAsString)) { // 4
+                answer = rom[romCounter] + rom[romCounter + 1] + answer;
+            } else if (numbers.get(2).contains(currentCharAsString)) { // 5, 6, 7, 8
+                answer = rom[romCounter + 1]
+                    + rom[romCounter].repeat(Integer.parseInt(String.valueOf(reversedString[i])) - FIVE) + answer;
+            } else if (numbers.get(THREE).contains(currentCharAsString)) { // 9
+                answer = rom[romCounter] + rom[romCounter + 2] + answer;
+            }
+
+            romCounter += 2;
+        }
+
+        return answer;
+    }
+}
---
+++ src/test/java/edu/TestTask4.java
@@ -0,0 +1,42 @@
+package edu;
+
+import org.junit.jupiter.api.Test;
+import static edu.Task4.convertToRoman;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestTask4 {
+
+    @Test
+    void TestConvert_2(){
+        int number = 2;
+
+        String res = convertToRoman(number);
+
+        assertThat(res).isEqualTo("II");
+    }
+    @Test
+    void TestConvert_12(){
+        int number = 12;
+
+        String res = convertToRoman(number);
+
+        assertThat(res).isEqualTo("XII");
+    }
+    @Test
+    void TestConvert_16(){
+        int number = 16;
+
+        String res = convertToRoman(number);
+
+        assertThat(res).isEqualTo("XVI");
+    }
+    @Test
+    void TestConvert_10(){
+        int number = 10;
+
+        String res = convertToRoman(number);
+
+        assertThat(res).isEqualTo("X");
+    }
+
+}
