7ac34cd8df82a73c3686a380abccac1c3af7fdb8
LZTD1 <danil227pavlov@gmail.com>
2023-11-22 00:08:49 +0300

    Реализация 3 задания

--- src/main/java/edu/hw7/Task3/SynchronizedDatabase.java
+++ src/main/java/edu/hw7/Task3/SynchronizedDatabase.java
@@ -4,74 +4,42 @@ import java.util.HashMap;
 import org.jetbrains.annotations.Nullable;
 
 public class SynchronizedDatabase implements PersonDatabase {
-
-    private final HashMap<String, Person> phoneBook;
-    private final HashMap<String, Person> addressBook;
-    private final HashMap<String, Person> nameBook;
-    private final HashMap<Integer, Person> idBook; // for quick remove person from other dict
-
-    public SynchronizedDatabase() {
-        this.nameBook = new HashMap<>();
-        this.phoneBook = new HashMap<>();
-        this.addressBook = new HashMap<>();
-        this.idBook = new HashMap<>();
-    }
+    private final HashMap<String, Person> nameBook = new HashMap<>();
+    private final HashMap<String, Person> phoneBook = new HashMap<>();
+    private final HashMap<String, Person> addressBook = new HashMap<>();
+    private final HashMap<Integer, Person> idBook = new HashMap<>();
 
     @Override
     public synchronized void add(Person person) {
-        updateBooks(person);
+        nameBook.put(person.name(), person);
+        phoneBook.put(person.phoneNumber(), person);
+        addressBook.put(person.address(), person);
+        idBook.put(person.id(), person);
     }
 
     @Override
     public synchronized void delete(int id) {
-        deletePersonById(id);
+        Person person = idBook.get(id);
+        if (person != null) {
+            nameBook.remove(person.name());
+            phoneBook.remove(person.phoneNumber());
+            addressBook.remove(person.address());
+            idBook.remove(id);
+        }
     }
 
     @Override
     public synchronized @Nullable Person findByName(String name) {
-        return this.nameBook.get(name);
+        return nameBook.get(name);
     }
 
     @Override
     public synchronized @Nullable Person findByAddress(String address) {
-        return this.addressBook.get(address);
+        return addressBook.get(address);
     }
 
     @Override
     public synchronized @Nullable Person findByPhone(String phone) {
-        return this.phoneBook.get(phone);
-    }
-
-    private void deletePersonById(int id) {
-        Person person = this.idBook.get(id);
-        if (person != null) {
-            this.nameBook.remove(person.name());
-            this.phoneBook.remove(person.phoneNumber());
-            this.addressBook.remove(person.address());
-            this.idBook.remove(id);
-        }
-    }
-
-    private void updateBooks(Person person) {
-        updateNameBook(person);
-        updatePhoneBook(person);
-        updateAddressBook(person);
-        updateIdBook(person);
-    }
-
-    private void updateIdBook(Person person) {
-        this.idBook.put(person.id(), person);
-    }
-
-    private void updateNameBook(Person person) {
-        this.nameBook.put(person.name(), person);
-    }
-
-    private void updatePhoneBook(Person person) {
-        this.phoneBook.put(person.phoneNumber(), person);
-    }
-
-    private void updateAddressBook(Person person) {
-        this.addressBook.put(person.address(), person);
+        return phoneBook.get(phone);
     }
 }
--- src/test/java/edu/hw7/TestTask3.java
+++ src/test/java/edu/hw7/TestTask3.java
@@ -4,12 +4,12 @@ import edu.hw7.Task3.NotSynchronizedDatabase;
 import edu.hw7.Task3.Person;
 import edu.hw7.Task3.PersonDatabase;
 import edu.hw7.Task3.SynchronizedDatabase;
+import edu.hw7.Task3.Task35.ReadWriteLockDatabase;
 import edu.umd.cs.mtc.MultithreadedTestCase;
 import edu.umd.cs.mtc.TestFramework;
+import java.util.Objects;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
-import java.util.Objects;
 
 public class TestTask3 {
     private final Person MOCK_PERSON = new Person(
@@ -18,6 +18,7 @@ public class TestTask3 {
         "Moscow",
         "89998887766"
     );
+    private final int COUNT_REPEAT = 300;
 
     @Nested
     class TestNotSynchronized extends MultithreadedTestCase {
@@ -60,7 +61,7 @@ public class TestTask3 {
 
         @Test
         public void testGetter() throws Throwable {
-            TestFramework.runManyTimes(new TestTask3.TestNotSynchronized(), 300);
+            TestFramework.runManyTimes(new TestTask3.TestNotSynchronized(), COUNT_REPEAT);
         }
     }
 
@@ -88,21 +89,14 @@ public class TestTask3 {
 
         @Override
         public void finish() {
-            System.out.println("+==================+");
-            System.out.println(foundByAddress);
-            System.out.println(foundByName);
-            System.out.println(foundByPhone);
-            System.out.println("+==================+");
-            boolean result;
-            if (foundByAddress == null && foundByName == null && foundByPhone == null){
-                 result = true;
-            } else if (Objects.equals(foundByAddress.address(), MOCK_PERSON.address())
-                && Objects.equals(foundByName.name(), MOCK_PERSON.name())
-                && Objects.equals(foundByPhone.phoneNumber(), MOCK_PERSON.phoneNumber())) {
-                 result = true;
+            boolean result = false;
 
-            }else{
-                 result = false;
+            if(foundByAddress != null){ // Если первый НЕ null
+                if(foundByName != null && foundByPhone != null){ // то и другие должны быть не null
+                    result = true;
+                }
+            }else{ // Если первый null - то не важно какие другие
+                result = true;
             }
 
             assertTrue(result);
@@ -110,7 +104,52 @@ public class TestTask3 {
 
         @Test
         public void testGetterSync() throws Throwable {
-            TestFramework.runManyTimes(new TestTask3.TestSynchronized(), 300);
+            TestFramework.runManyTimes(new TestTask3.TestSynchronized(), COUNT_REPEAT);
         }
     }
+
+    @Nested
+    class TestWithReadWriteLock extends MultithreadedTestCase {
+        private PersonDatabase myDatabase;
+        private Person foundByName;
+        private Person foundByPhone;
+        private Person foundByAddress;
+
+        @Override
+        public void initialize() {
+            myDatabase = new ReadWriteLockDatabase();
+        }
+
+        public void thread1() throws InterruptedException {
+            myDatabase.add(MOCK_PERSON);
+        }
+
+        public void thread2() throws InterruptedException {
+            foundByAddress = myDatabase.findByAddress(MOCK_PERSON.address());
+            foundByName = myDatabase.findByName(MOCK_PERSON.name());
+            foundByPhone = myDatabase.findByPhone(MOCK_PERSON.phoneNumber());
+        }
+
+        @Override
+        public void finish() {
+            boolean result = false;
+
+            if(foundByAddress != null){ // Если первый НЕ null
+                if(foundByName != null && foundByPhone != null){ // то и другие должны быть не null
+                    result = true;
+                }
+            }else{ // Если первый null - то не важно какие другие
+                result = true;
+            }
+
+            assertTrue(result);
+        }
+
+        @Test
+        public void testGetterSync() throws Throwable {
+            TestFramework.runManyTimes(new TestTask3.TestWithReadWriteLock(), COUNT_REPEAT);
+        }
+    }
+
 }
+
