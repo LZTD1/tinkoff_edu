a779bcfdb799412d2e28d5cecf8e8c23c3c4b296
LZTD1 <danil227pavlov@gmail.com>
2023-11-23 14:12:28 +0300

    2 задание | Реализован пул

---
+++ src/test/java/edu/hw8/TestTask2.java
@@ -0,0 +1,14 @@
+package edu.hw8;
+
+import edu.hw8.Task2.MyNewFixedThreadPool;
+import org.junit.jupiter.api.Test;
+
+public class TestTask2 {
+    private final
+
+    @Test
+    void testThrPool() throws Exception {
+        try(var pool = new MyNewFixedThreadPool(5)){
+        }
+    }
+}
